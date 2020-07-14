32d98c5e2998d518d37eced8e0b9bc51b0e45996
Inventas <inventas15@gmail.com>
2020-07-14 11:09:42 +0200

    Add VEdge to the project

---
+++ src/main/java/osl2/visualizer/model/VEdge.java
@@ -0,0 +1,10 @@
+public class VEdge {
+    private VNode start;
+    private VNode end;
+
+    public VEdge(VNode start, VNode end) {
+        this.start = start;
+        this.end = end;
+    }
+}
+
