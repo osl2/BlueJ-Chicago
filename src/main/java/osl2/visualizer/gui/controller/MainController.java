38ceb8bd8c360f37a2713a5e34fa321fda531509
Ralf Herp <65463376+feuerente@users.noreply.github.com>
2020-07-19 14:17:03 +0200

    Add MainController with interface

---
+++ src/main/java/osl2/visualizer/gui/controller/MainController.java
@@ -0,0 +1,66 @@
+package osl2.visualizer.gui.controller;
+
+import javafx.stage.Stage;
+import osl2.Chicago.ChicagoDatastructure;
+import osl2.visualizer.gui.MainView;
+import osl2.visualizer.gui.mirror.IMirrorController;
+import osl2.visualizer.gui.mirror.MirrorController;
+import osl2.visualizer.model.command.ICommandManager;
+
+/**
+ * Implementation of the {@link IMainController} of MVC.
+ */
+public class MainController implements IMainController {
+	private final MainView mainView;
+	private final ICommandManager commandManager;
+
+	private boolean isPlaying = false;
+
+	/**
+	 * Create a new MainController.
+	 *
+	 * @param commandManager - the {@link ICommandManager} to be used
+	 */
+	public MainController(ICommandManager commandManager) {
+		this.commandManager = commandManager;
+		DatastructureManager.registerController(this);
+		mainView = new MainView(this);
+		startView();
+	}
+
+	private void startView() {
+		try {
+			mainView.start(new Stage());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public void setPlayspeed(int playspeed) {
+		commandManager.setPlayspeed(playspeed);
+	}
+
+	@Override
+	public void playOrPause() {
+		if (isPlaying) {
+			commandManager.stopAutoPlay();
+			isPlaying = false;
+		} else {
+			commandManager.startAutoPlay();
+			isPlaying = true;
+		}
+	}
+
+	@Override
+	public void playStep() {
+		commandManager.playStep();
+	}
+
+	@Override
+	public void registerDatastructure(ChicagoDatastructure chicagoDatastructure) {
+		IMirrorController mirrorController = new MirrorController();
+		mainView.addMirrorButton(mirrorController.getMirrorButton());
+		mainView.addMirrorView(mirrorController.getMirrorView());
+	}
+}
