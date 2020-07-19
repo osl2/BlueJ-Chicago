19d3ebf774b2ea49ebc3afbe91caeea43931388d
Ralf Herp <65463376+feuerente@users.noreply.github.com>
2020-07-19 14:26:35 +0200

    Add stand ins: MirrorView and MirrorController

---
+++ src/main/java/osl2/visualizer/gui/mirror/MirrorController.java
@@ -0,0 +1,36 @@
+package osl2.visualizer.gui.mirror;
+
+/**
+ * Stand in until real MirrorController is done.
+ */
+public class MirrorController implements IMirrorController {
+	@Override
+	public void hideMirror() {
+
+	}
+
+	@Override
+	public void maximizeMirror() {
+
+	}
+
+	@Override
+	public void minimizeMirror() {
+
+	}
+
+	@Override
+	public void mirrorBtnKlicked() {
+
+	}
+
+	@Override
+	public MirrorButton getMirrorButton() {
+		return null;
+	}
+
+	@Override
+	public MirrorView getMirrorView() {
+		return null;
+	}
+}
---
+++ src/main/java/osl2/visualizer/gui/mirror/MirrorView.java
@@ -0,0 +1,7 @@
+package osl2.visualizer.gui.mirror;
+
+/**
+ * Stand in until real MirrorView is done.
+ */
+public class MirrorView {
+}
