ae80b3e8705c894be9ec931e1d76912d1abc7254
Ralf Herp <65463376+feuerente@users.noreply.github.com>
2020-07-21 13:47:39 +0200

    Add PropertyChange functionality to VisualLinkedLists

--- src/main/java/osl2/visualizer/model/VisualDoublyLinkedList.java
+++ src/main/java/osl2/visualizer/model/VisualDoublyLinkedList.java
@@ -1,9 +1,18 @@
 package osl2.visualizer.model;
 
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+
 public class VisualDoublyLinkedList<T> extends VisualList<T> {
+	private final PropertyChangeSupport pcs = new PropertyChangeSupport(this);
 
 	@Override
 	public Datastructure getDatastructureType() {
 		return Datastructure.DS_DOUBLY_LINKED_LIST;
+	}
+
+	@Override
+	public void addPropertyChangeListener(PropertyChangeListener listener) {
+		pcs.addPropertyChangeListener(listener);
 	}
 }
--- src/main/java/osl2/visualizer/model/VisualSinglyLinkedList.java
+++ src/main/java/osl2/visualizer/model/VisualSinglyLinkedList.java
@@ -1,9 +1,18 @@
 package osl2.visualizer.model;
 
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+
 public class VisualSinglyLinkedList<T> extends VisualList<T> {
+	private final PropertyChangeSupport pcs = new PropertyChangeSupport(this);
 
 	@Override
 	public Datastructure getDatastructureType() {
 		return Datastructure.DS_SINGLY_LINKED_LIST;
+	}
+
+	@Override
+	public void addPropertyChangeListener(PropertyChangeListener listener) {
+		pcs.addPropertyChangeListener(listener);
 	}
 }
