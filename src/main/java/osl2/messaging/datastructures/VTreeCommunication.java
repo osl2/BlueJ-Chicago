2929397b0ce719c36313f81f23816a3d51767348
ukqid <ukqid@student.kit.edu>
2020-08-08 16:53:45 +0200

    Add VTree class

---
+++ src/main/java/osl2/messaging/datastructures/VTreeCommunication.java
@@ -0,0 +1,7 @@
+package osl2.messaging.datastructures;
+
+import osl2.messaging.datastructures.nodey.VNodeyDatastructureCommunication;
+
+public interface VTreeCommunication<T> extends VNodeyDatastructureCommunication<T, VGraphNodeCommunication<T>> {
+
+}
