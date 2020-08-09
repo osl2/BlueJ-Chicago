26623445b80b6e31000018291379bcc0aab8f1d6
ucuxt <ucuxt@student.kit.edu>
2020-08-09 17:14:40 +0200

    Created Evanston.start() to make it easier to initialize our library

--- src/main/java/osl2/Evanston.java
+++ src/main/java/osl2/Evanston.java
@@ -21,8 +21,12 @@ public class Evanston {
         return EvanstonWindow.getInstance().getPlayController();
     }
 
-    public static void main(String[] args) {
+    public static void start() {
         EvanstonWindow.open();
+    }
+
+    public static void main(String[] args) {
+        start();
 
         VTree<Integer> tree = new VTree<>("Tree");
         VGraphNode node = tree.addTreeNode();
